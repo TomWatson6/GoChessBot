f73756daec99a2c51b693831437b22a1dea82ecf
TomWatson6 <37339340+TomWatson6@users.noreply.github.com>
2022-04-03 14:08:12 +0100

    Added simple implementation of chess package

--- cmd/chess/chess.go
+++ cmd/chess/chess.go
@@ -1,7 +1,16 @@
 package chess
 
-import "github.com/tomwatson6/chessbot/cmd/board"
+import (
+	"github.com/tomwatson6/chessbot/cmd/board"
+	"github.com/tomwatson6/chessbot/cmd/move"
+	"github.com/tomwatson6/chessbot/colour"
+)
 
 type Chess struct {
 	Board board.Board
+	Turn  colour.Colour
+}
+
+func (c *Chess) MakeMove(m move.Move) error {
+	return nil
 }
