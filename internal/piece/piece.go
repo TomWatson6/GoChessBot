1982cbce1055eeadc345f6052d512d531b4f047b
TomWatson6 <37339340+TomWatson6@users.noreply.github.com>
2022-04-03 15:04:07 +0100

    Added more requirements in the piece.go file

--- internal/piece/piece.go
+++ internal/piece/piece.go
@@ -3,6 +3,7 @@ package piece
 import (
 	"github.com/tomwatson6/chessbot/internal/ai/power"
 	"github.com/tomwatson6/chessbot/internal/colour"
+	"github.com/tomwatson6/chessbot/internal/move"
 )
 
 type Piece interface {
@@ -10,6 +11,7 @@ type Piece interface {
 	GetColour() colour.Colour
 	GetPower() power.Power
 	GetThreatLevel() int
+	IsValidMove(m move.Move) bool
 }
 
 type PieceType byte
@@ -32,4 +34,15 @@ const (
 	PiecePointsRook   PiecePoints = 5
 	PiecePointsQueen  PiecePoints = 9
 	PiecePointsKing   PiecePoints = 100
+)
+
+type PieceLetter rune
+
+const (
+	PieceLetterPawn   PieceLetter = 'P'
+	PieceLetterKnight PieceLetter = 'N'
+	PieceLetterBishop PieceLetter = 'B'
+	PieceLetterRook   PieceLetter = 'R'
+	PieceLetterQueen  PieceLetter = 'Q'
+	PieceLetterKing   PieceLetter = 'K'
 )
