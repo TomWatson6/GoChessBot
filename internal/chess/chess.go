81eb5053ab66212912dd4d44b3e69830160cbdf4
TomWatson6 <37339340+TomWatson6@users.noreply.github.com>
2022-04-12 17:10:02 +0100

    Implemented MakeMove() and added NextTurn() to chess.go

--- internal/chess/chess.go
+++ internal/chess/chess.go
@@ -1,6 +1,8 @@
 package chess
 
 import (
+	"fmt"
+
 	"github.com/tomwatson6/chessbot/internal/board"
 	"github.com/tomwatson6/chessbot/internal/colour"
 	"github.com/tomwatson6/chessbot/internal/move"
@@ -12,5 +14,17 @@ type Chess struct {
 }
 
 func (c *Chess) MakeMove(m move.Move) error {
-	return nil
+	if err := c.Board.MovePiece(m); err != nil {
+		return fmt.Errorf("invalid move: %v", m)
+	} else {
+		return nil
+	}
+}
+
+func (c *Chess) NextTurn() {
+	if c.Turn == colour.White {
+		c.Turn = colour.Black
+	} else {
+		c.Turn = colour.White
+	}
 }
