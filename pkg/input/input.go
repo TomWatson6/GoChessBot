2aa1a352d8c67796354bd141138f67a4bc3adeb4
tom.watson <tom.watson@skybettingandgaming.com>
2022-05-05 16:00:18 +0100

    Updated input to accept deletions separately to alterations

--- pkg/input/input.go
+++ pkg/input/input.go
@@ -8,7 +8,7 @@ import (
 	"github.com/tomwatson6/chessbot/internal/piece"
 )
 
-func Get(c colour.Colour, alterations map[move.Position]piece.Piece) chess.Chess {
+func Get(c colour.Colour, alterations map[move.Position]piece.Piece, deletions []move.Position) chess.Chess {
 	// Make board for standard game of chess
 
 	var squares []move.Position
@@ -22,38 +22,38 @@ func Get(c colour.Colour, alterations map[move.Position]piece.Piece) chess.Chess
 	pieces := make(map[move.Position]piece.Piece)
 
 	piecesList := []piece.Piece{
-		{Colour: colour.White, Position: move.Position{File: 0, Rank: 0}, PieceDetails: piece.Rook{}},
-		{Colour: colour.White, Position: move.Position{File: 1, Rank: 0}, PieceDetails: piece.Knight{}},
-		{Colour: colour.White, Position: move.Position{File: 2, Rank: 0}, PieceDetails: piece.Bishop{}},
-		{Colour: colour.White, Position: move.Position{File: 3, Rank: 0}, PieceDetails: piece.Queen{}},
-		{Colour: colour.White, Position: move.Position{File: 4, Rank: 0}, PieceDetails: piece.King{}},
-		{Colour: colour.White, Position: move.Position{File: 5, Rank: 0}, PieceDetails: piece.Bishop{}},
-		{Colour: colour.White, Position: move.Position{File: 6, Rank: 0}, PieceDetails: piece.Knight{}},
-		{Colour: colour.White, Position: move.Position{File: 7, Rank: 0}, PieceDetails: piece.Rook{}},
-		{Colour: colour.White, Position: move.Position{File: 0, Rank: 1}, PieceDetails: piece.Pawn{}},
-		{Colour: colour.White, Position: move.Position{File: 1, Rank: 1}, PieceDetails: piece.Pawn{}},
-		{Colour: colour.White, Position: move.Position{File: 2, Rank: 1}, PieceDetails: piece.Pawn{}},
-		{Colour: colour.White, Position: move.Position{File: 3, Rank: 1}, PieceDetails: piece.Pawn{}},
-		{Colour: colour.White, Position: move.Position{File: 4, Rank: 1}, PieceDetails: piece.Pawn{}},
-		{Colour: colour.White, Position: move.Position{File: 5, Rank: 1}, PieceDetails: piece.Pawn{}},
-		{Colour: colour.White, Position: move.Position{File: 6, Rank: 1}, PieceDetails: piece.Pawn{}},
-		{Colour: colour.White, Position: move.Position{File: 7, Rank: 1}, PieceDetails: piece.Pawn{}},
-		{Colour: colour.Black, Position: move.Position{File: 0, Rank: 7}, PieceDetails: piece.Rook{}},
-		{Colour: colour.Black, Position: move.Position{File: 1, Rank: 7}, PieceDetails: piece.Knight{}},
-		{Colour: colour.Black, Position: move.Position{File: 2, Rank: 7}, PieceDetails: piece.Bishop{}},
-		{Colour: colour.Black, Position: move.Position{File: 3, Rank: 7}, PieceDetails: piece.Queen{}},
-		{Colour: colour.Black, Position: move.Position{File: 4, Rank: 7}, PieceDetails: piece.King{}},
-		{Colour: colour.Black, Position: move.Position{File: 5, Rank: 7}, PieceDetails: piece.Bishop{}},
-		{Colour: colour.Black, Position: move.Position{File: 6, Rank: 7}, PieceDetails: piece.Knight{}},
-		{Colour: colour.Black, Position: move.Position{File: 7, Rank: 7}, PieceDetails: piece.Rook{}},
-		{Colour: colour.Black, Position: move.Position{File: 0, Rank: 6}, PieceDetails: piece.Pawn{}},
-		{Colour: colour.Black, Position: move.Position{File: 1, Rank: 6}, PieceDetails: piece.Pawn{}},
-		{Colour: colour.Black, Position: move.Position{File: 2, Rank: 6}, PieceDetails: piece.Pawn{}},
-		{Colour: colour.Black, Position: move.Position{File: 3, Rank: 6}, PieceDetails: piece.Pawn{}},
-		{Colour: colour.Black, Position: move.Position{File: 4, Rank: 6}, PieceDetails: piece.Pawn{}},
-		{Colour: colour.Black, Position: move.Position{File: 5, Rank: 6}, PieceDetails: piece.Pawn{}},
-		{Colour: colour.Black, Position: move.Position{File: 6, Rank: 6}, PieceDetails: piece.Pawn{}},
-		{Colour: colour.Black, Position: move.Position{File: 7, Rank: 6}, PieceDetails: piece.Pawn{}},
+		{Colour: colour.White, Position: move.Position{File: 0, Rank: 0}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Rook{}},
+		{Colour: colour.White, Position: move.Position{File: 1, Rank: 0}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Knight{}},
+		{Colour: colour.White, Position: move.Position{File: 2, Rank: 0}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Bishop{}},
+		{Colour: colour.White, Position: move.Position{File: 3, Rank: 0}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Queen{}},
+		{Colour: colour.White, Position: move.Position{File: 4, Rank: 0}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.King{}},
+		{Colour: colour.White, Position: move.Position{File: 5, Rank: 0}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Bishop{}},
+		{Colour: colour.White, Position: move.Position{File: 6, Rank: 0}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Knight{}},
+		{Colour: colour.White, Position: move.Position{File: 7, Rank: 0}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Rook{}},
+		{Colour: colour.White, Position: move.Position{File: 0, Rank: 1}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Pawn{}},
+		{Colour: colour.White, Position: move.Position{File: 1, Rank: 1}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Pawn{}},
+		{Colour: colour.White, Position: move.Position{File: 2, Rank: 1}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Pawn{}},
+		{Colour: colour.White, Position: move.Position{File: 3, Rank: 1}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Pawn{}},
+		{Colour: colour.White, Position: move.Position{File: 4, Rank: 1}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Pawn{}},
+		{Colour: colour.White, Position: move.Position{File: 5, Rank: 1}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Pawn{}},
+		{Colour: colour.White, Position: move.Position{File: 6, Rank: 1}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Pawn{}},
+		{Colour: colour.White, Position: move.Position{File: 7, Rank: 1}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Pawn{}},
+		{Colour: colour.Black, Position: move.Position{File: 0, Rank: 7}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Rook{}},
+		{Colour: colour.Black, Position: move.Position{File: 1, Rank: 7}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Knight{}},
+		{Colour: colour.Black, Position: move.Position{File: 2, Rank: 7}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Bishop{}},
+		{Colour: colour.Black, Position: move.Position{File: 3, Rank: 7}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Queen{}},
+		{Colour: colour.Black, Position: move.Position{File: 4, Rank: 7}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.King{}},
+		{Colour: colour.Black, Position: move.Position{File: 5, Rank: 7}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Bishop{}},
+		{Colour: colour.Black, Position: move.Position{File: 6, Rank: 7}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Knight{}},
+		{Colour: colour.Black, Position: move.Position{File: 7, Rank: 7}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Rook{}},
+		{Colour: colour.Black, Position: move.Position{File: 0, Rank: 6}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Pawn{}},
+		{Colour: colour.Black, Position: move.Position{File: 1, Rank: 6}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Pawn{}},
+		{Colour: colour.Black, Position: move.Position{File: 2, Rank: 6}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Pawn{}},
+		{Colour: colour.Black, Position: move.Position{File: 3, Rank: 6}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Pawn{}},
+		{Colour: colour.Black, Position: move.Position{File: 4, Rank: 6}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Pawn{}},
+		{Colour: colour.Black, Position: move.Position{File: 5, Rank: 6}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Pawn{}},
+		{Colour: colour.Black, Position: move.Position{File: 6, Rank: 6}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Pawn{}},
+		{Colour: colour.Black, Position: move.Position{File: 7, Rank: 6}, ValidMoves: make(map[move.Position]bool), PieceDetails: piece.Pawn{}},
 	}
 
 	for _, p := range piecesList {
@@ -61,11 +61,11 @@ func Get(c colour.Colour, alterations map[move.Position]piece.Piece) chess.Chess
 	}
 
 	for k, v := range alterations {
-		if v.Equals(piece.Piece{}) {
-			delete(pieces, k)
-		} else {
-			pieces[k] = v
-		}
+		pieces[k] = v
+	}
+
+	for _, d := range deletions {
+		delete(pieces, d)
 	}
 
 	chess := chess.Chess{
